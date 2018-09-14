58f3bdba5ee8a2a5126142c0c4183e6f3a0c0593
ecreyes <ereyesoyarzo@gmail.com>
2018-09-13 21:27:16 -0300

    CRUD Finalizado

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -4,7 +4,49 @@ class ArticlesController < ApplicationController
     end
 
     def show
+        contador = Article.find(params[:id]).visits_count
+        if(!contador)
+            contador = 0
+        end
+        contador = contador +1
+        Article.find(params[:id]).update(visits_count:contador)
         @article = Article.find(params[:id])
-        puts params
+    end
+
+    def new
+        @article = Article.new
+    end
+
+    def create
+        @article = Article.new(article_params)
+        if @article.save
+            redirect_to articles_path
+        else
+            render :new
+        end
+    end
+
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        redirect_to articles_path
+    end
+
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(article_params)
+            redirect_to @article
+        else
+            render :edit
+        end
+    end
+
+    private
+    def article_params
+        params.require(:article).permit(:title,:body)
     end
 end
